de4e2cf591565822e570cf2d08357cd0502d98cf
AlexFilonenko88 <af88_88@mail.ru>
2023-11-11 21:01:38 +0300

    add task array

---  eternal_form.php
+++  eternal_form.php
@@ -3,22 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <?php
 $filename = __DIR__ . '\text.txt';
 
-$file='';
-if(file_exists($filename)){
-        $file = file_get_contents($filename);
+$text = '';
+if(isset($_POST['text'])){
+    $text = $_POST['text'];
 }
-file_put_contents($filename, $_POST['text']);
+
+$file = '';
+if (file_exists($filename)) {
+    $file = file_get_contents($filename);
+    file_put_contents($filename, $text);
+}
+
 ?>
 <body>
 <form method="POST">
-    <p><textarea name="text" rows="10" cols="45"><?= htmlspecialchars(isset($file)?$file:'') ?> </textarea></p>
+    <p><textarea name="text" rows="10" cols="45"><?= htmlspecialchars(isset($file) ? $file : '') ?> </textarea></p>
     <input type="submit" value="Отправить">
 </form>
 </body>
